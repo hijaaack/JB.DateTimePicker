b20d937a0fe225dfd8960eacb25a275cba65b950
Jack Borelius <JackB@beckhoff.com>
2022-01-18 12:56:41 +0100

    First commit

---
+++ DateTimePicker/DateTimePicker/DateTimePickerControl/DateTimePickerControl.js
@@ -0,0 +1,131 @@
+/*
+ * Generated 12/7/2020 3:39:04 PM
+ * Copyright (C) 2020
+ */
+///<reference path="../node_modules/@types/daterangepicker/index.d.ts" />
+///<reference path="../node_modules/moment/moment.d.ts" />
+var TcHmi;
+(function (TcHmi) {
+    let Controls;
+    (function (Controls) {
+        let DateTimePicker;
+        (function (DateTimePicker) {
+            class DateTimePickerControl extends TcHmi.Controls.System.TcHmiControl {
+                /*
+                Attribute philosophy
+                --------------------
+                - Local variables are not set while definition in class, so they have the value 'undefined'.
+                - On compile the Framework sets the value from HTML or from theme (possibly 'null') via normal setters
+                - The "changed detection" in the setter will result in processing the value only once while compile
+                - Attention: If we have a Server Binding on an Attribute the setter can be very late or never (leaving the value = undefined).
+                */
+                /**
+                 * @description Constructor of the control
+                 * @param {JQuery} element Element from HTML (internal, do not use)
+                 * @param {JQuery} pcElement precompiled Element (internal, do not use)
+                 * @param {TcHmi.Controls.ControlAttributeList} attrs Attributes defined in HTML in a special format (internal, do not use)
+                 * @returns {void}
+                 */
+                constructor(element, pcElement, attrs) {
+                    /** Call base class constructor */
+                    super(element, pcElement, attrs);
+                }
+                /**
+                  * If raised, the control object exists in control cache and constructor of each inheritation level was called.
+                  * Call attribute processor functions here to initialize default values!
+                  */
+                __previnit() {
+                    // Fetch template root element
+                    this.__elementTemplateRoot = this.__element.find('.daterangepicker-root');
+                    if (this.__elementTemplateRoot.length === 0) {
+                        throw new Error('Invalid Template.html');
+                    }
+                    this.__elementInput = this.__elementTemplateRoot.find('.daterangepicker-input');
+                    //TcHmi_Controls_DateTimePicker_DateTimePickerControl
+                    // Call __previnit of base class
+                    super.__previnit();
+                }
+                /**
+                 * @description Is called during control initialize phase after attribute setter have been called based on it's default or initial html dom values.
+                 * @returns {void}
+                 */
+                __init() {
+                    super.__init();
+                }
+                /**
+                * @description Is called by tachcontrol() after the control instance gets part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __attach() {
+                    super.__attach();
+                    /**
+                     * Initialize everything which is only available while the control is part of the active dom.
+                     */
+                    var $this = this;
+                    var input = this.__elementInput.get(0);
+                    this.__drp = new daterangepicker(input, {
+                        "showDropdowns": true,
+                        "showWeekNumbers": true,
+                        "timePicker": true,
+                        "timePicker24Hour": true,
+                        "timePickerSeconds": true,
+                        "minDate": "1970-01-01",
+                        "maxDate": "2106-02-06",
+                        "drops": "auto",
+                        locale: {
+                            format: 'YYYY-MM-DD HH:mm:ss'
+                        }
+                    }, function (start, end) {
+                        $this.__startDateTime = start.format('YYYY-MM-DDTHH:mm:ss');
+                        $this.__endDateTime = end.format('YYYY-MM-DDTHH:mm:ss');
+                        // inform the system that the function has a changed result.
+                        TcHmi.EventProvider.raise($this.__id + ".onFunctionResultChanged", ["getStartDateTime"]);
+                        TcHmi.EventProvider.raise($this.__id + ".onFunctionResultChanged", ["getEndDateTime"]);
+                    });
+                }
+                /**
+                * @description Is called by tachcontrol() after the control instance is no longer part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __detach() {
+                    super.__detach();
+                    /**
+                     * Disable everything which is not needed while the control is not part of the active dom.
+                     * No need to listen to events for example!
+                     */
+                }
+                /**
+                * @description Destroy the current control instance.
+                * Will be called automatically if system destroys control!
+                */
+                destroy() {
+                    /**
+                    * While __keepAlive is set to true control must not be destroyed.
+                    */
+                    if (this.__keepAlive) {
+                        return;
+                    }
+                    super.destroy();
+                    /**
+                    * Free resources like child controls etc.
+                    */
+                }
+                getStartDateTime() {
+                    return this.__startDateTime + "Z";
+                }
+                getEndDateTime() {
+                    return this.__endDateTime + "Z";
+                }
+                setStartDateTime(valueNew) {
+                    return;
+                }
+                setEndDateTime(valueNew) {
+                    return;
+                }
+            }
+            DateTimePicker.DateTimePickerControl = DateTimePickerControl;
+        })(DateTimePicker = Controls.DateTimePicker || (Controls.DateTimePicker = {}));
+        Controls.registerEx('DateTimePickerControl', 'TcHmi.Controls.DateTimePicker', DateTimePicker.DateTimePickerControl);
+    })(Controls = TcHmi.Controls || (TcHmi.Controls = {}));
+})(TcHmi || (TcHmi = {}));
+//# sourceMappingURL=DateTimePickerControl.js.map
---
+++ Readme.md
@@ -0,0 +1,22 @@
+﻿# JB.DateTimePicker for TwinCAT HMI
+
+Calender control that uses the [daterangepicker](https://github.com/dangrossman/daterangepicker)
+The control start and end datetime is formatted for the [DT/DATE_AND_TIME](https://infosys.beckhoff.com/content/1033/tcplccontrol/html/tcplcctrl_date_and_time.htm?id=1430897617053350049) PLC datatype.
+
+![enter image description here](https://user-images.githubusercontent.com/75740551/101644076-1088a380-3a35-11eb-8ed6-85a417fe7cd2.png)
+
+# Installation
+Easiest way to install this package is inside your TwinCAT HMI Project. 
+**Right click** References and click "Manage NuGet Packages.." then browse for the file and install it! 
+![enter image description here](https://user-images.githubusercontent.com/75740551/101645035-32cef100-3a36-11eb-88f4-eeaccd3366d6.png)
+
+# Configuration
+Declare two variables in the PLC with the type
+
+ - DT / DATE_AND_TIME
+
+Then create a databinding with **TwoWay** databinding.
+
+To change the binding mode, **Right Click** the symbol and chose **Edit symbol..**
+![enter image description here](https://user-images.githubusercontent.com/75740551/101644297-4ded3100-3a35-11eb-88b7-ec03df8b6ee6.png)
+
